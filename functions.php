cb42a6a50f8832b500c416804e2200389a7bc2c1
daniel martins <freitasdaniel39@gmail.com>
2020-11-07 21:14:30 -0300

    renomeando rota

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 $template_diretorio = get_template_directory();
 
-require_once($template_diretorio . '/custom-post-type/produto.php');
+require_once($template_diretorio . '/custom-post-type/estados.php');
 
 require_once($template_diretorio . '/endpoints/estados_get.php');
 require_once($template_diretorio . '/endpoints/estado_get.php');
